61eea4ac69cf8109e3a0eade51bb21dceb881585
ptaejoon <neo.flix@kakaocorp.com>
2022-07-10 00:00:54 +0900

    add todo, sample server

---
+++ experiment-scheduler/common/TODO.md
@@ -0,0 +1,5 @@
+1. Make Convention On Communication (Response, Request)
+2. Create Proto
+3. Create GRPC Code
+4. Build GRPC Class
+
--- experiment-scheduler/common/grpc_server.py
+++ experiment-scheduler/common/grpc_server.py
@@ -1,7 +1,7 @@
 import grpc
 from common.master_task_manager_pb2_grpc import SubmitterCommandServiceStub as scss
 from common import master_task_manager_pb2 as pb2
-
+from common import master_task_manager_pb2_grpc as pb2_grpc
 
 class GrpcServer:
     def __init__(self):
@@ -11,9 +11,15 @@ class GrpcServer:
     def start_server(self):
         pass
 
-    def run(self):
+    def run_command(self):
         with grpc.insecure_channel('localhost:50051') as channel:
             stub = scss(channel)
             response = stub.run_experiments(pb2.SubmitterRequest(name="abc"))
             print(response.message)
 
+    def run_server(self):
+        server = grpc.server(4)
+        pb2_grpc.add_SubmitterCommandServiceServicer_to_server(pb2_grpc.SubmitterCommandServiceServicer(),server)
+        server.add_insecure_port()
+        server.start()
+        server.wait_for_termination()
