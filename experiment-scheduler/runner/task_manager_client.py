0e4b3dee7a05c8c2a67dd86a83345c427459246d
Eunwoo, Shin <sew9869@gmail.com>
2022-07-10 22:14:01 +0900

    make task_manager structure

---
+++ experiment-scheduler/runner/task_manager_client.py
@@ -0,0 +1,57 @@
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Python implementation of the gRPC route guide client."""
+
+from __future__ import print_function
+
+import logging
+import random
+
+import grpc
+import task_manager_pb2
+import task_manager_pb2_grpc
+
+
+def make_route_note(message, latitude, longitude):
+    return route_guide_pb2.RouteNote(
+        message=message,
+        location=route_guide_pb2.Point(latitude=latitude, longitude=longitude))
+
+
+def guide_get_one_feature(stub, point):
+    feature = stub.RunTask(point)
+    print("task_id", feature.task_id)
+    print("response", feature.response)
+
+
+def guide_get_feature(stub):
+    guide_get_one_feature(
+        stub, task_manager_pb2.TaskStatement(gpuidx=7, command="run hpo", name="something"))
+
+
+
+def run():
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = task_manager_pb2_grpc.TaskManagerStub(channel)
+        print("-------------- RunTask --------------")
+        guide_get_feature(stub)
+        print("DONE!")
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
---
+++ experiment-scheduler/runner/task_manager_server.py
@@ -0,0 +1,59 @@
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Python implementation of the gRPC route guide server."""
+
+from concurrent import futures
+import logging
+import math
+import time
+
+import grpc
+import task_manager_pb2
+import task_manager_pb2_grpc
+
+
+class TaskManagerServicer(task_manager_pb2_grpc.TaskManagerServicer):
+    """Provides methods that implement functionality of route guide server."""
+
+    def __init__(self):
+        pass
+
+    def RunTask(self, request, context):
+        print("gpuidx", request.gpuidx)
+        print("command", request.command)
+        print("name", request.name)
+        return task_manager_pb2.Response(task_id="123", response=2)
+
+    def KillTask(self, request, context):
+        pass
+
+    def GetTaskStatus(self, request_iterator, context):
+        pass        
+
+    def GetAllTasks(self, request_iterator, context):
+        pass
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    task_manager_pb2_grpc.add_TaskManagerServicer_to_server(
+        RouteGuideServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
