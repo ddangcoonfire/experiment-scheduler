fd8557f404367cb18ad5e562be76a6fdbd3121f4
ptaejoon <neo.flix@kakaocorp.com>
2022-08-07 09:49:24 +0900

    test code

--- test/master/master.py
+++ test/master/master.py
@@ -1,19 +1,25 @@
 import sys
-sys.path.append("./")
 
+sys.path.append("./")
+import time
 from experiment_scheduler.master.master import Master
 import grpc
 from concurrent import futures
-from experiment_scheduler.master.grpc_master.master_pb2_grpc import add_MasterServicer_to_server,MasterStub
-from experiment_scheduler.master.grpc_master.master_pb2 import ExperimentStatement, MasterTaskStatement, MasterTaskCondition
-from experiment_scheduler.task_manager.task_manager_pb2_grpc import add_TaskManagerServicer_to_server, TaskManagerStub
-from experiment_scheduler.task_manager.task_manager_pb2 import TaskManager
+from experiment_scheduler.master.grpc_master.master_pb2_grpc import add_MasterServicer_to_server, MasterStub
+from experiment_scheduler.master.grpc_master.master_pb2 import ExperimentStatement, MasterTaskStatement, \
+    MasterTaskCondition
+from experiment_scheduler.task_manager.grpc_task_manager.task_manager_pb2_grpc import add_TaskManagerServicer_to_server, \
+    TaskManagerStub
+from experiment_scheduler.task_manager.task_manager_server import TaskManagerServicer
+from multiprocessing import Process, set_start_method
+import logging
 
 
 def turn_master_on():
     master = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
     add_MasterServicer_to_server(Master(), master)
-    master.add_insecure_port('[::]:50050')
+    master.add_insecure_port('[::]:50051')
+    print("turn master",flush=True)
     master.start()
     master.wait_for_termination()
 
@@ -21,22 +27,40 @@ def turn_master_on():
 def turn_task_manager_on():
     task_manager = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
     add_TaskManagerServicer_to_server(TaskManagerServicer(), task_manager)
-    task_manager.add_insecure_port('[::]:50051')
+    task_manager.add_insecure_port('[::]:50052')
+    print("turn manager")
     task_manager.start()
     task_manager.wait_for_termination()
 
 
 class MasterTester:
     def __init__(self):
-        turn_master_on()
-        turn_task_manager_on()
-        self.task_manager_stub = TaskManagerStub(grpc.insecure_channel("localhost:50051"))
-        self.master_stub = MasterStub(grpc.insecure_channel("localhost:50050"))
+        master_process = Process(target=turn_master_on, )
+        master_process.start()
+        task_manager_process = Process(target=turn_task_manager_on, )
+        task_manager_process.start()
+        time.sleep(1)
+        self.task_manager_stub = TaskManagerStub(grpc.insecure_channel("localhost:50052"))
+        self.master_stub = MasterStub(grpc.insecure_channel("localhost:50051"))
 
     def test_master(self):
-        protobuf = ExperimentStatement(name="test1",
-                                       tasks=MasterTaskStatement(command="cmd1", name="name1",
-                                                           condition=MasterTaskCondition(gpuidx=0)))
-        response = self.master_stub.request_experiment(protobuf)
-        assert type(response) is int
+        master_task_statement = MasterTaskStatement()
+        master_task_statement.command="cmd1"
+        master_task_statement.name="name1"
+        master_task_statement.task_env["a"] = "b"
+        master_task_statement.condition.MergeFrom(MasterTaskCondition(gpuidx=0))
+        protobuf = ExperimentStatement(name="test1", tasks=[master_task_statement])
+        response = self.master_stub.request_experiments(protobuf)
+        print(response)
+        print(type(response))
+        assert type(response) is str
 
+    def run_unit_test(self):
+        pass
+
+
+if __name__ == "__main__":
+    # set_start_method('spawn')
+    tester = MasterTester()
+    tester.run_unit_test()
+    tester.test_master()
