287d9e36f900f3c0785259c367c1d39b9d122445
Sangmin0728 <cscscs0728@gmail.com>
2022-08-22 20:56:22 +0900

    refact: moduling task_id

--- test/task_manager/test_task_manager_kill_task.py
+++ test/task_manager/test_task_manager_kill_task.py
@@ -5,6 +5,7 @@ import pytest
 from experiment_scheduler.task_manager import task_manager_server
 from experiment_scheduler.task_manager.grpc_task_manager import task_manager_pb2, task_manager_pb2_grpc
 
+
 class MockTask:
     def __init__(self, task_id):
         self.task_id = task_id
@@ -21,9 +22,11 @@ class MockTask:
     def wait():
         return None
 
+
 class MockRunTask(MockTask):
     def __init__(self, task_id):
         self.task_id = task_id
+
 
 class MockDoneTask(MockTask):
     def __init__(self, task_id):
@@ -33,6 +36,7 @@ class MockDoneTask(MockTask):
     def poll():
         return 0
 
+
 class MockKilledTask(MockTask):
     def __init__(self, task_id):
         self.task_id = task_id
@@ -41,62 +45,70 @@ class MockKilledTask(MockTask):
     def poll():
         return -signal.SIGTERM
 
+
 class MockRequest:
     def __init__(self, task_id):
         self.task_id = task_id
 
+
+class mock_task_id:
+    def __init__(self):
+        self.status = {'RUNNING': '1', 'DONE': '2', 'KILLED': '3', 'NOTFOUND': '4'}
+
+
 class TestClass:
-    TaskID = Enum('task_id', {'RUNNING': '1', 'DONE': '2', 'KILLED': '3', 'NOTFOUND': '4'})
+
+    task_id = mock_task_id()
 
     @pytest.fixture
     def task_manager_servicer(self):
-        task_id = Enum('task_id', {'RUNNING': '1', 'DONE': '2', 'KILLED': '3', 'NOTFOUND': '4'})
+        task_id = mock_task_id()
         task_manager_servicer = task_manager_server.TaskManagerServicer(task_manager_pb2_grpc.TaskManagerServicer)
-        task_manager_servicer.tasks[task_id.RUNNING.value] = MockRunTask(task_id.RUNNING.value)
-        task_manager_servicer.tasks[task_id.DONE.value] = MockDoneTask(task_id.DONE.value)
-        task_manager_servicer.tasks[task_id.KILLED.value] = MockKilledTask(task_id.KILLED.value)
+        task_manager_servicer.tasks[task_id.status['RUNNING']] = MockRunTask(task_id.status['RUNNING'])
+        task_manager_servicer.tasks[task_id.status['DONE']] = MockDoneTask(task_id.status['DONE'])
+        task_manager_servicer.tasks[task_id.status['KILLED']] = MockKilledTask(task_id.status['KILLED'])
         return task_manager_servicer
 
-    @pytest.mark.parametrize('requested', [MockRequest(TaskID.NOTFOUND.value)])
+    @pytest.mark.parametrize('requested', [MockRequest(task_id.status['NOTFOUND'])])
     def test_kill_not_found_task(self, requested, task_manager_servicer):
         assert task_manager_servicer.kill_task(requested, '') == task_manager_pb2.TaskStatus(task_id=requested.task_id,
                                                                                              status=task_manager_pb2.
                                                                                              TaskStatus.Status.NOTFOUND)
 
-    @pytest.mark.parametrize('requested', [MockRequest(TaskID.DONE.value)])
+    @pytest.mark.parametrize('requested', [MockRequest(task_id.status['DONE'])])
     def test_kill_done_task(self, requested, task_manager_servicer):
         assert task_manager_servicer.kill_task(requested, '') == task_manager_pb2.TaskStatus(task_id=requested.task_id,
                                                                                              status=task_manager_pb2.
                                                                                              TaskStatus.Status.DONE)
 
-    @pytest.mark.parametrize('requested', [MockRequest(TaskID.RUNNING.value)])
+    @pytest.mark.parametrize('requested', [MockRequest(task_id.status['RUNNING'])])
     def test_kill_run_task(self, requested, task_manager_servicer):
         assert task_manager_servicer.kill_task(requested, '') == task_manager_pb2.TaskStatus(task_id=requested.task_id,
                                                                                              status=task_manager_pb2.
                                                                                              TaskStatus.Status.KILLED)
 
-    @pytest.mark.parametrize('requested', [MockRequest(TaskID.NOTFOUND.value)])
+    @pytest.mark.parametrize('requested', [MockRequest(task_id.status['NOTFOUND'])])
     def test_get_not_found_task_status(self, requested, task_manager_servicer):
         assert task_manager_servicer.get_task_status(requested, '') == task_manager_pb2.TaskStatus(
             task_id=requested.task_id,
             status=task_manager_pb2.
             TaskStatus.Status.NOTFOUND)
 
-    @pytest.mark.parametrize('requested', [MockRequest(TaskID.RUNNING.value)])
+    @pytest.mark.parametrize('requested', [MockRequest(task_id.status['RUNNING'])])
     def test_get_running_task_status(self, requested, task_manager_servicer):
         assert task_manager_servicer.get_task_status(requested, '') == task_manager_pb2.TaskStatus(
             task_id=requested.task_id,
             status=task_manager_pb2.
             TaskStatus.Status.RUNNING)
 
-    @pytest.mark.parametrize('requested', [MockRequest(TaskID.DONE.value)])
+    @pytest.mark.parametrize('requested', [MockRequest(task_id.status['DONE'])])
     def test_get_done_task_status(self, requested, task_manager_servicer):
         assert task_manager_servicer.get_task_status(requested, '') == task_manager_pb2.TaskStatus(
             task_id=requested.task_id,
             status=task_manager_pb2.
             TaskStatus.Status.DONE)
 
-    @pytest.mark.parametrize('requested', [MockRequest(TaskID.KILLED.value)])
+    @pytest.mark.parametrize('requested', [MockRequest(task_id.status['KILLED'])])
     def test_get_killed_task_status(self, requested, task_manager_servicer):
         assert task_manager_servicer.get_task_status(requested, '') == task_manager_pb2.TaskStatus(
             task_id=requested.task_id,
@@ -104,39 +116,39 @@ class TestClass:
             TaskStatus.Status.KILLED)
 
     def test_get_all_tasks(self, task_manager_servicer):
-        task_id = Enum('task_id', {'RUNNING': '1', 'DONE': '2', 'KILLED': '3', 'NOTFOUND': '4'})
+        task_id = mock_task_id()
         expect_all_tasks_status = task_manager_pb2.AllTasksStatus()
-        expect_all_tasks_status.task_status_array.append(task_manager_pb2.TaskStatus(task_id=task_id.RUNNING.value,
+        expect_all_tasks_status.task_status_array.append(task_manager_pb2.TaskStatus(task_id=task_id.status['RUNNING'],
                                                                                      status=task_manager_pb2.TaskStatus.Status.RUNNING))
         expect_all_tasks_status.task_status_array.append(
-            task_manager_pb2.TaskStatus(task_id=task_id.DONE.value, status=task_manager_pb2.TaskStatus.Status.DONE))
+            task_manager_pb2.TaskStatus(task_id=task_id.status['DONE'], status=task_manager_pb2.TaskStatus.Status.DONE))
         expect_all_tasks_status.task_status_array.append(
-            task_manager_pb2.TaskStatus(task_id=task_id.KILLED.value, status=task_manager_pb2.TaskStatus.Status.KILLED))
+            task_manager_pb2.TaskStatus(task_id=task_id.status['KILLED'], status=task_manager_pb2.TaskStatus.Status.KILLED))
         assert task_manager_servicer.get_all_tasks(None, '') == expect_all_tasks_status
 
-    @pytest.mark.parametrize('request_task_id', TaskID.RUNNING.value)
+    @pytest.mark.parametrize('request_task_id', task_id.status['RUNNING'])
     def test_wrap_by_grpc_task_status_running_task(self, request_task_id, task_manager_servicer):
         assert task_manager_servicer._wrap_by_grpc_TaskStatus(request_task_id) == task_manager_pb2.TaskStatus(
             task_id=request_task_id, status=task_manager_pb2.TaskStatus.Status.RUNNING)
 
-    @pytest.mark.parametrize('request_task_id', TaskID.DONE.value)
+    @pytest.mark.parametrize('request_task_id', task_id.status['DONE'])
     def test_wrap_by_grpc_task_status_done_task(self, request_task_id, task_manager_servicer):
         assert task_manager_servicer._wrap_by_grpc_TaskStatus(request_task_id) == task_manager_pb2.TaskStatus(
             task_id=request_task_id, status=task_manager_pb2.TaskStatus.Status.DONE)
 
-    @pytest.mark.parametrize('request_task_id', TaskID.KILLED.value)
+    @pytest.mark.parametrize('request_task_id', task_id.status['KILLED'])
     def test_wrap_by_grpc_task_status_killed_task(self, request_task_id, task_manager_servicer):
         assert task_manager_servicer._wrap_by_grpc_TaskStatus(request_task_id) == task_manager_pb2.TaskStatus(
             task_id=request_task_id, status=task_manager_pb2.TaskStatus.Status.KILLED)
 
-    @pytest.mark.parametrize('request_task_id', TaskID.RUNNING.value)
+    @pytest.mark.parametrize('request_task_id', task_id.status['RUNNING'])
     def test_get_task_running_task(self, request_task_id, task_manager_servicer):
         assert task_manager_servicer._get_task(request_task_id) == task_manager_servicer.tasks[request_task_id]
 
-    @pytest.mark.parametrize('request_task_id', TaskID.DONE.value)
+    @pytest.mark.parametrize('request_task_id', task_id.status['DONE'])
     def test_get_task_done_task(self, request_task_id, task_manager_servicer):
         assert task_manager_servicer._get_task(request_task_id) == task_manager_servicer.tasks[request_task_id]
 
-    @pytest.mark.parametrize('request_task_id', TaskID.KILLED.value)
+    @pytest.mark.parametrize('request_task_id', task_id.status['KILLED'])
     def test_get_task_killed_task(self, request_task_id, task_manager_servicer):
         assert task_manager_servicer._get_task(request_task_id) == task_manager_servicer.tasks[request_task_id]
