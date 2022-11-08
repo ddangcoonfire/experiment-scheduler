ec4ecff894bd820b6217c6f5a82de21de0d20452
Armstrong-SuHwan <01034317974a@gmail.com>
2022-11-09 00:16:37 +0900

    feat: add submitter about delete/status/all_tasks

--- experiment_scheduler/submitter/list.py
+++ experiment_scheduler/submitter/list.py
@@ -1,4 +1,23 @@
+"""
+[TODO] exs list command Explanation
+"""
+import ast
+import grpc
+from experiment_scheduler.common.settings import USER_CONFIG
+from experiment_scheduler.master.grpc_master import master_pb2_grpc, master_pb2
+
+
 def main():
-    raise NotImplementedError
+    """
+    Todo
+    :return:
+    """
+    channel = grpc.insecure_channel(
+        ast.literal_eval(USER_CONFIG.get("default", "master_address"))
+    )
+    stub = master_pb2_grpc.MasterStub(channel)
+    request = master_pb2.google_dot_protobuf_dot_empty__pb2.Empty()
+    response = stub.get_all_tasks(request)
 
-
+    for task in response:
+        print(task)
