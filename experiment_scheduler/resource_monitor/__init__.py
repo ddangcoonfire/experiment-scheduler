9c1d9d79a9f38367e48aa1c0e77e862b67c55d48
Eunwoo, Shin <sew9869@gmail.com>
2023-03-16 20:25:38 +0900

    move resource manager class and remove unused resource manager file

---
+++ experiment_scheduler/resource_monitor/__init__.py
@@ -0,0 +1,3 @@
+from .resource_manager import ResourceManager
+
+all = ["ResourceManager"]
--- experiment_scheduler/task_manager/task_manager_server.py
+++ experiment_scheduler/task_manager/task_manager_server.py
@@ -24,6 +24,7 @@ from experiment_scheduler.common.logging import get_logger, start_end_logger
 
 logger = get_logger(name="task_manager")
 from experiment_scheduler.task_manager.return_code import return_code
+from experiment_scheduler.resource_monitor import ResourceManager
 from experiment_scheduler.common.settings import USER_CONFIG
 
 KILL_CHILD_MAX_DEPTH = 2
@@ -93,50 +94,6 @@ class ProcessUtil:
             process.wait()
 
         return True
-
-
-class ResourceManager:
-    def __init__(self, num_resource: int):
-        self._resource_rental_history = {}
-        self._available_resources = [True for _ in range(num_resource)]
-        self.logger = get_logger(name="resource_manager")
-
-    def set_resource_as_idle(self, resource_idx: int):
-        self._available_resources[resource_idx] = True
-
-    def set_resource_as_used(self, resource_idx: int):
-        self._available_resources[resource_idx] = False
-
-    def release_resource(self, task_id):
-        if task_id not in self._resource_rental_history:
-            return
-        resource_idx = self._resource_rental_history[task_id]
-        self._available_resources[resource_idx] = True
-        del self._resource_rental_history[task_id]
-
-    def get_resource(self, task_id):
-        resource_idx = None
-        for idx, resource_is_idle in enumerate(self._available_resources):
-            if resource_is_idle:
-                resource_idx = idx
-                break
-        if resource_idx is None:
-            self.logger.info("There isn't any available resource.")
-            return None
-
-        self._resource_rental_history[task_id] = resource_idx
-        self._available_resources[resource_idx] = False
-
-        return resource_idx
-
-    def has_available_resource(self):
-        for resource_is_idle in self._available_resources:
-            if resource_is_idle:
-                return True
-        return False
-
-    def get_tasks_using_resource(self):
-        return list(self._resource_rental_history.keys())
 
 
 class TaskManagerServicer(task_manager_pb2_grpc.TaskManagerServicer, return_code):
