6517f73072a592013ac916c52aead705cc006ae9
Eunwoo, Shin <sew9869@gmail.com>
2023-03-16 20:25:38 +0900

    move resource manager class and remove unused resource manager file

--- experiment_scheduler/task_manager/task_manager_server.py
+++ experiment_scheduler/task_manager/task_manager_server.py
@@ -22,6 +22,7 @@ from experiment_scheduler.task_manager.grpc_task_manager.task_manager_pb2 import
     TaskStatus,
 )
 from experiment_scheduler.task_manager.return_code import ReturnCode
+from experiment_scheduler.resource_monitor import ResourceManager
 
 logger = get_logger(name="task_manager")
 
@@ -98,83 +99,7 @@ class ProcessUtil:
         return True
 
 
-class ResourceManager:
-    """
-    Resource Manager checks task manager's status.
-    """
-
-    def __init__(self, num_resource: int):
-        self._resource_rental_history = {}
-        self._available_resources = [True for _ in range(num_resource)]
-        self.logger = get_logger(name="resource_manager")
-
-    def set_resource_as_idle(self, resource_idx: int):
-        """
-
-        :param resource_idx:
-        :return:
-        """
-        self._available_resources[resource_idx] = True
-
-    def set_resource_as_used(self, resource_idx: int):
-        """
-
-        :param resource_idx:
-        :return:
-        """
-        self._available_resources[resource_idx] = False
-
-    def release_resource(self, task_id):
-        """
-
-        :param task_id:
-        :return:
-        """
-        if task_id not in self._resource_rental_history:
-            return
-        resource_idx = self._resource_rental_history[task_id]
-        self._available_resources[resource_idx] = True
-        del self._resource_rental_history[task_id]
-
-    def get_resource(self, task_id):
-        """
-
-        :param task_id:
-        :return:
-        """
-        resource_idx = None
-        for idx, resource_is_idle in enumerate(self._available_resources):
-            if resource_is_idle:
-                resource_idx = idx
-                break
-        if resource_idx is None:
-            self.logger.info("There isn't any available resource.")
-            return None
-
-        self._resource_rental_history[task_id] = resource_idx
-        self._available_resources[resource_idx] = False
-
-        return resource_idx
-
-    def has_available_resource(self):
-        """
-
-        :return:
-        """
-        for resource_is_idle in self._available_resources:
-            if resource_is_idle:
-                return True
-        return False
-
-    def get_tasks_using_resource(self):
-        """
-
-        :return:
-        """
-        return list(self._resource_rental_history.keys())
-
-
-class TaskManagerServicer(task_manager_pb2_grpc.TaskManagerServicer, ReturnCode):
+class TaskManagerServicer(task_manager_pb2_grpc.TaskManagerServicer, return_code):
     """Provides methods that implement functionality of task manager server."""
 
     # pylint: disable=no-member
