9112f0dd749c8dbadbb3ed89f5b58d0f8c5a2463
Eunwoo, Shin <sew9869@gmail.com>
2023-03-22 23:17:57 +0900

    refactor list.py

--- experiment_scheduler/submitter/list.py
+++ experiment_scheduler/submitter/list.py
@@ -11,6 +11,21 @@ from experiment_scheduler.common.settings import USER_CONFIG
 from experiment_scheduler.master.grpc_master import master_pb2, master_pb2_grpc
 
 TASK_STATUS = ["waiting", "running", "done", "killed", "abnormal", "not found"]
+TERMINAL_COL_SIZE = os.get_terminal_size().columns
+
+
+def _get_id_format_string(id: str, verbose: bool) -> str:
+    if verbose:
+        return f"{id:{TERMINAL_COL_SIZE - 20}}"
+    if TERMINAL_COL_SIZE > 120:
+        length = 100
+    else:
+        length = TERMINAL_COL_SIZE - 20
+    return f"{id[:70]:{length}}"
+
+
+def _get_task_format_string(task_id: str, status, verbose: bool) -> str:
+    return f"{_get_id_format_string(task_id, verbose)} ({TASK_STATUS[status]})"
 
 
 def parse_args():
@@ -31,43 +46,16 @@ def main():
     :return:
     """
     args = parse_args()
-    ter_col_size = os.get_terminal_size().columns
 
-    channel = grpc.insecure_channel(
-        ast.literal_eval(USER_CONFIG.get("default", "master_address"))
-    )
+    channel = grpc.insecure_channel(ast.literal_eval(USER_CONFIG.get("default", "master_address")))
     stub = master_pb2_grpc.MasterStub(channel)
     request = master_pb2.Experiment(experiment_id=args.experiment)
     response = stub.get_all_tasks(request)
 
-    # print(response)
-
-    if args.verbose:
-        for exp_status in response.experiment_status_array:
-            exp_array_without_last = exp_status.task_status_array.task_status_array[:-1]
-            exp_array_last = exp_status.task_status_array.task_status_array[-1]
-            print(f"experiment_id: {exp_status.experiment_id:{ter_col_size - 20 }}")
-            for task_status in exp_array_without_last:
-                print(
-                    f"\t├─ {task_status.task_id:{ter_col_size - 20 }} ({TASK_STATUS[task_status.status]})"
-                )
-            print(
-                f"\t└─ {exp_array_last.task_id:{ter_col_size - 20 }} ({TASK_STATUS[exp_array_last.status]})"
-            )
-    else:
-        for exp_status in response.experiment_status_array:
-            exp_array_without_last = exp_status.task_status_array.task_status_array[:-1]
-            exp_array_last = exp_status.task_status_array.task_status_array[-1]
-            print(
-                f"experiment_id: "
-                f"{exp_status.experiment_id[:70]:{100 if ter_col_size - 20 > 100 else ter_col_size - 20}}"
-            )
-            for task_status in exp_array_without_last:
-                print(
-                    f"\t├─ {task_status.task_id[:70]:{100 if ter_col_size - 20 > 100 else ter_col_size - 20}} "
-                    f"({TASK_STATUS[task_status.status]})"
-                )
-            print(
-                f"\t└─ {exp_array_last.task_id[:70]:{100 if ter_col_size - 20 > 100 else ter_col_size - 20}} "
-                f"({TASK_STATUS[exp_array_last.status]})"
-            )
+    for exp_status in response.experiment_status_array:
+        exp_array_without_last = exp_status.task_status_array.task_status_array[:-1]
+        exp_array_last = exp_status.task_status_array.task_status_array[-1]
+        print(f"experiment_id: {_get_id_format_string(exp_status.experiment_id, args.verbose)}")
+        for task_status in exp_array_without_last:
+            print(f"\t├─ {_get_task_format_string(task_status.task_id, task_status.status, args.verbose)}")
+        print(f"\t└─ {_get_id_format_string(exp_array_last.task_id, exp_array_last.status, args.verbose)}")
