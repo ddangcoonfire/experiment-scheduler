04e65027851627eea7c3a0a2fd18b8c3b13bb0db
Hankyu Jang <hankyujang@Hankyuui-noteubug.local>
2023-04-27 20:23:31 +0900

    test_init

--- test/task_manager/test_task_manager_v2.py
+++ test/task_manager/test_task_manager_v2.py
@@ -10,7 +10,7 @@ from experiment_scheduler.task_manager.grpc_task_manager.task_manager_pb2 import
     ServerStatus,
     TaskStatement,
 )
-from experiment_scheduler.task_manager.task_manager_server import TaskManagerServicer
+from experiment_scheduler.task_manager.task_manager_server import TaskManagerServicer, ResourceManager
 
 
 class MockTask:
@@ -26,11 +26,18 @@ class TestTaskManager:
     @pytest.fixture(autouse=True)
     def init(self, mocker):
         mocker.patch.object(task_manager_server, "Thread")
+        self.mock_pynvml = mocker.patch.object(task_manager_server, "pynvml")
+        self.mock_pynvml.nvmlDeviceGetCount.return_value = 4
         self.mock_resource_manager = mocker.MagicMock()
-        mocker.patch.object(task_manager_server, "ResourceManager", return_value=self.mock_resource_manager)
+        self.mock_resource_manager_class = mocker.patch.object(task_manager_server, "ResourceManager", return_value=self.mock_resource_manager)
         self.task_manger_server = TaskManagerServicer()
 
+    def test_init(self, mocker):
+        self.mock_resource_manager_class.assert_called_once_with(4)
+
+
     def test_get_dead_tasks(self, mocker):
+
         self.task_manger_server.tasks.update([
             ("1", MockTask(1)), ("2", MockTask(2, 1)), ("3", MockTask(3, None)),
         ])
