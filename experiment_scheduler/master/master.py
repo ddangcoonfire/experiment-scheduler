d9bfefdf0e1ec2627fd74a112c4b69fe0685deca
SeokMin <ksm012015@naver.com>
2023-05-04 20:43:55 +0900

    [DDAN-104] master test code (#74)
    Co-authored-by: ptaejoon <gomataejoon@gmail.com>

--- experiment_scheduler/master/master.py
+++ experiment_scheduler/master/master.py
@@ -169,6 +169,7 @@ class Master(MasterServicer):
             else response_status.FAIL
         )
         # [todo] add task_id
+        # [TODO] Add exception for failed state while requesting experiment.
         ExperimentEntity.insert(exp)
         return MasterResponse(experiment_id=experiment_id, response=response)
 
@@ -306,7 +307,7 @@ class Master(MasterServicer):
                     )
                 all_experiments_status.experiment_status_array.append(
                     ExperimentsStatus(
-                        experiment_id=request.experiment_id,
+                        experiment_id=exp.id,
                         task_status_array=all_tasks_status,
                     )
                 )
